d68626edbe49519fa8e2ebc280ed8da13f35e193
200402537 <200402537@student.georgianc.on.ca>
2019-09-20 11:18:45 -0400

    Updated findsetidfiles.sh

--- Bash/findsetidfiles.sh
+++ Bash/findsetidfiles.sh
@@ -12,4 +12,13 @@
 echo "Setuid files:"
 echo "============="
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
-echo ""
+echo""
+
+echo "Setgid files:"
+echo "============="
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
+echo""
+
+echo "10 Largest files:"
+echo "============="
+find /home/ -type f -exec ls -alh --block-size=M {} \; | sort -hr -k5 | head -n 10 | awk '{print $5, $3, $9}'
